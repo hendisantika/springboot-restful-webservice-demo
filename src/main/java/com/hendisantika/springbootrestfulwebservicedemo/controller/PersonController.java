3e142338906de75fa86bd615c470014873e14fea
hendisantika <hendisantika@yahoo.co.id>
2019-07-16 06:24:16 +0700

    #22 - Add getHome Method in PersonController

--- src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.hendisantika.springbootrestfulwebservicedemo.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,10 @@ public class PersonController {
 
     @Autowired
     private PersonService personService;
+
+    @GetMapping(value = "/")
+    public String getHome() {
+        return "My name is Uzumaki Naruto";
+    }
+
 }
