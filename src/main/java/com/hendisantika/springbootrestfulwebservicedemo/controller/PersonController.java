f346f89254e2ed07bfa4d012be2ca6ba19514c20
hendisantika <hendisantika@yahoo.co.id>
2019-07-16 06:25:04 +0700

    #23 - Add getAllPersons Method in PersonController

--- src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
@@ -1,12 +1,17 @@
 package com.hendisantika.springbootrestfulwebservicedemo.controller;
 
+import com.hendisantika.springbootrestfulwebservicedemo.model.Person;
 import com.hendisantika.springbootrestfulwebservicedemo.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,16 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 06:23
  */
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/")
 public class PersonController {
     private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
 
     @Autowired
     private PersonService personService;
 
-    @GetMapping(value = "/")
+    @GetMapping
     public String getHome() {
         return "My name is Uzumaki Naruto";
+    }
+
+    @GetMapping("persons")
+    public ResponseEntity<List<Person>> getAllPersons() {
+        logger.info("All persons listed");
+        List<Person> persons = personService.getAllPerson();
+        return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
     }
 
 }
