2140708c57ad5a5357975a4cd5c1d0ce2b882b15
hendisantika <hendisantika@yahoo.co.id>
2019-07-16 06:25:49 +0700

    #24 - Add getPersonById Method in PersonController

--- src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
@@ -1,13 +1,16 @@
 package com.hendisantika.springbootrestfulwebservicedemo.controller;
 
+import com.hendisantika.springbootrestfulwebservicedemo.exception.response.CustomException;
 import com.hendisantika.springbootrestfulwebservicedemo.model.Person;
 import com.hendisantika.springbootrestfulwebservicedemo.service.PersonService;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +43,16 @@ public class PersonController {
         logger.info("All persons listed");
         List<Person> persons = personService.getAllPerson();
         return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+    }
+
+    @GetMapping("persons/{id}")
+    public ResponseEntity<Person> getPersonById(
+            @ApiParam(value = "Id of person", required = true) @PathVariable("id") long id) throws CustomException {
+        Person person = personService.getPersonById(id).get();
+        if (person == null) {
+            throw new CustomException("Person with provided id:" + id + " not found");
+        }
+        return new ResponseEntity<Person>(person, HttpStatus.OK);
     }
 
 }
