4cc676eb0b3e9983f46a0b1334ecb948fa2773c8
hendisantika <hendisantika@yahoo.co.id>
2019-07-16 06:26:34 +0700

    #25 - Add removePersonById Method in PersonController

--- src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/controller/PersonController.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootrestfulwebservicedemo.controller;
 
 import com.hendisantika.springbootrestfulwebservicedemo.exception.response.CustomException;
 import com.hendisantika.springbootrestfulwebservicedemo.model.Person;
+import com.hendisantika.springbootrestfulwebservicedemo.response.Response;
 import com.hendisantika.springbootrestfulwebservicedemo.service.PersonService;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
@@ -9,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,6 +51,20 @@ public class PersonController {
             throw new CustomException("Person with provided id:" + id + " not found");
         }
         return new ResponseEntity<Person>(person, HttpStatus.OK);
+    }
+
+    @DeleteMapping("persons/{id}")
+    public ResponseEntity<Response> removePersonById(
+            @ApiParam(value = "Id of person to be deleted", required = true) @PathVariable("id") long id)
+            throws CustomException {
+        Person person = personService.getPersonById(id).get();
+        if (person == null) {
+            throw new CustomException("Person with provided id:" + id + " cannot be deleted");
+        }
+        personService.removePerson(person);
+        logger.info("Person deleted successfully");
+        Response response = new Response(HttpStatus.OK.value(), "Person has been deleted");
+        return new ResponseEntity<Response>(response, HttpStatus.NO_CONTENT);
     }
 
 }
