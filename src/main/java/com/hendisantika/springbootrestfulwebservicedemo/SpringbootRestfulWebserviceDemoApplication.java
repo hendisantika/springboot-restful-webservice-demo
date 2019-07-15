7fa3f366734d11390f122914c5286d6b5cc60da3
hendisantika <hendisantika@yahoo.co.id>
2019-07-16 05:42:32 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/SpringbootRestfulWebserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrestfulwebservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulWebserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulWebserviceDemoApplication.class, args);
+    }
+
+}
