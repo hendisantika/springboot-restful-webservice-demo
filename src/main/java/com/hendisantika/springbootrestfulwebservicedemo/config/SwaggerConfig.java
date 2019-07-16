82392911383fe71e6641b12feb7464c40ee8f34c
hendisantika <hendisantika@yahoo.co.id>
2019-07-17 06:13:17 +0700

    #29 - Add more detail in Swagger

--- src/main/java/com/hendisantika/springbootrestfulwebservicedemo/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootrestfulwebservicedemo/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -41,7 +42,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.webservice.test.restfulWebserviceTesting.controller"))
+                .apis(RequestHandlerSelectors.basePackage(" com.hendisantika.springbootrestfulwebservicedemo.controller"))
                 .paths(PathSelectors.ant("/**")).build();
 
         /* paths(PathSelectors.any()) */
@@ -54,9 +55,15 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title(this.apiInfoReference.getInfo("title"))
-                .description(this.apiInfoReference.getInfo("description"))
-                .version(this.apiInfoReference.getInfo("apiVersion")).build();
+        Contact contact = new Contact("Hendi Santika", "http://hendisantika.wordpress.com", "hendisantika@gmail.com");
+        return new ApiInfoBuilder()
+                .title("Spring Boot Restful Webservice Demo")
+                .description("Spring Boot Restful Webservice Demo")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
     }
 
 }
